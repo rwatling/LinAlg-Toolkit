ac069dcaae9ea18f9722a97fa1a7287b098f57cd
Robert Watling <rwatling0902@gmail.com>
2018-06-14 14:03:00 -0400

    Added a vector class

---
+++ Linear Algebra Toolkit/src/Vector.java
@@ -0,0 +1,13 @@
+
+public class Vector extends RationalMatrix {
+
+	public Vector() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vector(String[][] stringArray) {
+		super(stringArray);
+		// TODO Auto-generated constructor stub
+	}
+
+}
